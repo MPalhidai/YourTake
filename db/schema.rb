91bfd53e3236e2be44e064648c930a2743b93bdf
Michael Palhidai <mpalhidai@gmail.com>
2018-09-28 16:08:27 -0700

    Movie model generation

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_09_27_024153) do
+ActiveRecord::Schema.define(version: 2018_09_28_225906) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "movies", force: :cascade do |t|
+    t.string "title"
+    t.string "external_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
@@ -28,6 +35,7 @@ ActiveRecord::Schema.define(version: 2018_09_27_024153) do
     t.inet "last_sign_in_ip"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "name"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
---
+++ spec/factories/movies.rb
@@ -0,0 +1,6 @@
+FactoryBot.define do
+  factory :movie do
+    title { "MyString" }
+    external_id { "MyString" }
+  end
+end

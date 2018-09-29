9bb3254b760f73f34b1c2f9aea99311186c338ed
Michael Palhidai <mpalhidai@gmail.com>
2018-09-28 17:00:34 -0700

    added associations

--- app/models/review.rb
+++ app/models/review.rb
@@ -1,2 +1,4 @@
 class Review < ApplicationRecord
+  has_one :user, :dependent => :destroy
+  has_one :movie, :dependent => :destroy
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -2,4 +2,7 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable,
          :validatable, :confirmable, :lockable, :timeoutable
+
+  has_many :reviews, :dependent => :destroy
+
 end

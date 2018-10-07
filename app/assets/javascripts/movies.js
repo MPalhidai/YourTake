31f21916d5c44729660c3448cb271fd6cc75557b
Michael Palhidai <mpalhidai@gmail.com>
2018-10-07 09:39:16 -0700

    added padding and seperation in code

--- app/assets/javascripts/movies.js
+++ app/assets/javascripts/movies.js
@@ -3,10 +3,13 @@ const KEY = "?api_key=b93dbe1063e7a3c72594bd72ccddee56";
 const DISCOVER = "discover/movie";
 const GENRE = "genre/movie/list";
 const LANGUAGE = "&language=en-US";
-let movieIndexURL = BASEURL + DISCOVER + KEY + LANGUAGE + "&sort_by=popularity.desc&include_adult=false&include_video=false&page=1";
 const GENRES = BASEURL + GENRE + KEY + LANGUAGE;
 const MONTHS = {'01':'Jan','02':'Feb','03':'Mar','04':'Apr','05':'May','06':'Jun','07':'Jul','08':'Aug','09':'Sep','10':'Oct','11':'Nov','12':'Dec'};
+
+let movieIndexURL = BASEURL + DISCOVER + KEY + LANGUAGE + "&sort_by=popularity.desc&include_adult=false&include_video=false&page=1";
+
 let genreList = {};
+
 let settings = {
   async: true,
   crossDomain: true,
@@ -18,30 +21,41 @@ let settings = {
 
 const renderMovies = (movies) => {
   movies.results.forEach(function (movie) {
+
     let movie_partials = document.querySelector('.movie_partials');
+
     let movie_card = document.createElement('div');
     movie_card.classList.add('movie_card', 'border', 'border-secondary', 'p-3', 'mb-3');
+
     let movie_card_top_row = document.createElement('div');
     movie_card_top_row.classList.add('row', 'justify-content-between');
+
     let movie_card_top_col_4 = document.createElement('div');
     movie_card_top_col_4.classList.add('col-4', 'text-center');
+
     let movie_card_cover = document.createElement('div');
     movie_card_cover.classList.add('movie_card_cover');
+
     let movie_card_cover_img = document.createElement('img');
     movie_card_cover_img.classList.add('movie_card_cover_img');
     movie_card_cover_img.src = `http://image.tmdb.org/t/p/w154//${movie.poster_path}`;
+
     let movie_card_top_col_8 = document.createElement('div');
-    movie_card_top_col_8.classList.add('col-8', 'p-0');
+    movie_card_top_col_8.classList.add('col-8');
+
     let movie_card_title = document.createElement('h3');
     movie_card_title.classList.add('movie_card_title');
     movie_card_title.innerHTML = movie.title;
+
     let movie_card_release_date = document.createElement('p');
     movie_card_release_date.classList.add('m-0');
     let date = movie.release_date.split('-');
     movie_card_release_date.innerHTML = `Release Date: ${MONTHS[date[1]]} ${date[2]}, ${date[0]}`;
+
     let movie_card_genre = document.createElement('p');
     movie_card_genre.classList.add('mb-2');
     movie_card_genre.innerHTML = 'Genre: ';
+
     for (let i = 0; i < movie.genre_ids.length; i++) {
       if (i != movie.genre_ids.length - 1) {
         movie_card_genre.innerHTML += `${genreList[movie.genre_ids[i]]}, `;
@@ -49,45 +63,68 @@ const renderMovies = (movies) => {
         movie_card_genre.innerHTML += `${genreList[movie.genre_ids[i]]}`;
       }
     };
+
     let movie_card_rating_div = document.createElement('div');
-    movie_card_rating_div.classList.add('movie_card_reviews', 'mb-2');
+    movie_card_rating_div.classList.add('movie_card_reviews');
+
+    let movie_card_rating_div_hidden = document.createElement('div');
+    movie_card_rating_div_hidden.classList.add('movie_card_reviews_hidden', 'd-none');
+
     settings.url = BASEURL + `movie/${movie.id}/reviews`+ KEY + LANGUAGE + `&page=1`;
     $.ajax(settings).done(function (api_movie_review_call) {
       // api_movie_review_call.results.for 2 then extend to all on page 1
       for (let i = 0; i < api_movie_review_call.results.length; i++) {
+
         let movie_card_rating_stars = document.createElement('span');
-        movie_card_rating_stars.classList.add('stars-container', 'bigger', 'stars-' + (movie.vote_average*10).toString());
+        movie_card_rating_stars.classList.add('stars-container', 'stars-' + (movie.vote_average*10).toString());
         movie_card_rating_stars.innerHTML = '★★★★★';
+
         let movie_card_rating_partial = document.createElement('a');
         movie_card_rating_partial.classList.add('movie_card_rating', 'mb-0');
         movie_card_rating_partial.href = api_movie_review_call.results[i].url;
+
         let long_comment = api_movie_review_call.results[i].content;
-        if (long_comment.length > 32) {
-          let short_comment = long_comment.substring(0, 29) + "...";
+        if (long_comment.length > 35) {
+          let short_comment = long_comment.substring(0, 32) + "...";
           movie_card_rating_partial.innerHTML = ` ${short_comment} - ${api_movie_review_call.results[i].author}`;
         } else {
           movie_card_rating_partial.innerHTML = ` ${long_comment} - ${api_movie_review_call.results[i].author}`;
         }
-        movie_card_rating_div.appendChild(movie_card_rating_stars);
-        movie_card_rating_div.appendChild(movie_card_rating_partial);
-        movie_card_rating_div.appendChild(document.createElement('br'));
+
+        if (i < 2) {
+          movie_card_rating_div.appendChild(movie_card_rating_stars);
+          movie_card_rating_div.appendChild(movie_card_rating_partial);
+          movie_card_rating_div.appendChild(document.createElement('br'));
+        } else {
+          movie_card_rating_div_hidden.appendChild(movie_card_rating_stars);
+          movie_card_rating_div_hidden.appendChild(movie_card_rating_partial);
+          movie_card_rating_div_hidden.appendChild(document.createElement('br'));
+        }
       }
     });
+    
     let movie_card_bot_row = document.createElement('div');
     movie_card_bot_row.classList.add('row', 'align-items-end');
+
     let movie_card_bot_col_4 = document.createElement('div');
     movie_card_bot_col_4.classList.add('col-4', 'movie_card_rating_avg', 'text-center');
+
     let movie_card_star_span = document.createElement('span');
     movie_card_star_span.classList.add('stars-container', 'bigger', 'stars-' + (movie.vote_average*10).toString());
     movie_card_star_span.innerHTML = '★★★★★';
+
     let movie_card_bot_col_8 = document.createElement('div');
-    movie_card_bot_col_8.classList.add('col-8', 'p-0');
-    let movie_card_view_more = document.createElement('a');
-    movie_card_view_more.classList.add('movie_card_view_more', 'mr-2');
+    movie_card_bot_col_8.classList.add('col-8');
+
+    let movie_card_view_more = document.createElement('button');
+    movie_card_view_more.classList.add('movie_card_view_more', 'mr-2', 'p-0', 'btn', 'btn-link');
     movie_card_view_more.innerHTML = "View More";
-    // CHANGE ABOVE movie_card_view_more.href = movie/show;
-    let movie_card_leave_review = document.createElement('a');
-    movie_card_leave_review.classList.add('movie_card_leave_review', 'mx-2');
+    movie_card_view_more.addEventListener('click', () => {
+      movie_card_rating_div_hidden.classList.remove('d-none');
+    });
+
+    let movie_card_leave_review = document.createElement('button');
+    movie_card_leave_review.classList.add('movie_card_leave_review', 'mx-2', 'p-0', 'btn', 'btn-link');
     movie_card_leave_review.innerHTML = "Leave A Review";
     // CHANGE ABOVE movie_card_leave_review.href = movie/review; possibly open review form modal
 
@@ -99,6 +136,7 @@ const renderMovies = (movies) => {
     movie_card_top_col_8.appendChild(movie_card_release_date);
     movie_card_top_col_8.appendChild(movie_card_genre);
     movie_card_top_col_8.appendChild(movie_card_rating_div);
+    movie_card_top_col_8.appendChild(movie_card_rating_div_hidden);
 
     movie_card_bot_col_4.appendChild(movie_card_star_span);
 

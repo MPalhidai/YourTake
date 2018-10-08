5017e7ba4aa1477f765532c10a24a63a15b67ca0
Michael Palhidai <mpalhidai@gmail.com>
2018-10-08 15:22:49 -0700

    moved home view to about and root route to movies index page

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
-  root 'static_pages#home'
+  root "movies#index"
   devise_for :users
   resources :movies, only: [:index, :show]
   resources :reviews, except: [:edit, :update]
+  get "/static_pages/about" => "static_pages#about", as: "about"
 end

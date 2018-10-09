bfaf3aa0970f703cd9646818a257b243768050ef
Michael Palhidai <mpalhidai@gmail.com>
2018-10-09 12:04:13 -0700

    added review rating validation for 0-100

--- app/models/review.rb
+++ app/models/review.rb
@@ -3,4 +3,5 @@ class Review < ApplicationRecord
   has_one :movie, :dependent => :destroy
 
   validates :comment, presence: true
+  validates :rating, presence: true, inclusion: { in: 0..100, message: 'The rating must be between 0 and 100' }
 end
